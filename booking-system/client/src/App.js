8b5055cb9bd81d6d68419cf67c704795ec94ae28
dylan <dylanpjx@gmail.com>
2023-01-18 21:07:10 +0800

    Revert App.js to simple skeleton code

--- booking-system/client/src/App.js
+++ booking-system/client/src/App.js
@@ -3,21 +3,9 @@ import Calendar from './Calendar';
 import Login from './Login';
 
 function App() {
-    const [backendData, setBackendData] = useState([{}])
-
-    useEffect(() => {
-        fetch("api").then(
-            response => response.json()
-        ).then(
-            data => {
-                setBackendData(data)
-            }
-        )
-    }, [])
-
     return (
         <div>
-            <Login />
+            <Calendar />
         </div>
     );
 }
