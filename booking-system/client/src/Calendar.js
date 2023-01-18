2e6170128d98cf3b50934c7c2e90cc7e314b665d
dylan <dylanpjx@gmail.com>
2023-01-18 21:06:12 +0800

    Add all neccessary methods for Calendar.js

--- booking-system/client/src/Calendar.js
+++ booking-system/client/src/Calendar.js
@@ -1,36 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Scheduler } from '@aldabil/react-scheduler';
+
+const DB_URL = 'localhost:5000'
+
+
 const Calendar = () => {
 
-    const fetchRemote = async => () => {
+    const fetchRemote = async () => {
+        const events = await fetch(`${DB_URL}/calendar_data`);
+
+        return events;
     }
- 
-    const handleConfirm = async => (event, action) => {
-        if (action == "edit") {
-            /* PUT event to remote DB */
-        } else if (action == "create") {
-            /* POST event to remote DB */
+
+    const handleConfirm = async (event, action) => {
+        try {
+            let url;
+            let method;
+
+            if (action == "edit") {
+                url = `${DB_URL}/calendar_data/${event.event_id}`;
+                method = "PUT"
+            } else if (action == "create") {
+                url = `${DB_URL}/calendar_data/`;
+                method = "POST"
+            }
+
+            let body = JSON.stringify(event);
+            const res = await fetch(url, {
+                method,
+                headers: { "Content-Type": "application/json"},
+                body
+            });
+            const data = await res.json();
+            return data;
+
+        } catch(err) {
+            // should perform error handling here in future
+            console.error("Error:", err);
         }
-
-        return new Promise((res, rej) => {
-            
-        })
     }
 
-    const handleDelete = async => (deletedId) => {
-        return new Promise((res, rej) => {
-            setTimeout(() => {
-                res(deletedId)
-            }, 3000)
-        }) 
+    const handleDelete = async (deletedId) => {
+        try {
+            const res = await fetch (`${DB_URL}/calendar_data/${deletedId}`, {
+                method: "DELETE"
+            });
+            return await res.json();
+        } catch (err) {
+            console.error("Error:", err)
+        }
     }
 
     return (
         <div>
             <Scheduler
-                view="week"
-                navigation={false}
-                disableViewNavigator={true}
+                getRemoteEvents={fetchRemote}
+                onConfirm={handleConfirm}
+                onDelete={handleDelete}
             />
         </div>
     )
