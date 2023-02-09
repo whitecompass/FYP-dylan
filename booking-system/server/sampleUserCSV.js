b69f8ce814e9e526bb1908b8f929cf23004ce034
whitecompass <116876873+whitecompass@users.noreply.github.com>
2023-02-09 17:40:04 +0800

    Update sampleUserCSV.js

--- booking-system/server/sampleUserCSV.js
+++ booking-system/server/sampleUserCSV.js
@@ -1,17 +1,17 @@
 const fs = require("fs");
 
 const sampleData = [
-  { id: 1, name: 'Karen', module: 'ee2033', matricno: 'a0101234a', group: 2, username: 'karenthia', password: 'karenthia01' },
-  { id: 2, name: 'Bernerd', module: 'ee3131c', matricno: 'a0101234b', group: 1, username: 'bernerdtan', password: 'bernerdtan01' },
-  { id: 3, name: 'Karen', module: 'ee2033', matricno: 'a0101234c', group: 2, username: 'karenthia', password: 'karenthia01' },
-  { id: 4, name: 'Karen', module: 'ee4218', matricno: 'a0101234d', group: 2, username: 'karenthia', password: 'karenthia01' },
-  { id: 5, name: 'Karen', module: 'ee2033', matricno: 'a0101234e', group: 3, username: 'karenthia', password: 'karenthia01' }
+  { id: 1, name: 'Karen', module: 'ee2033', matricno: 'a0101234a', role:'student', group: 2, username: 'karenthia', password: 'karenthia01' },
+  { id: 2, name: 'Bernerd', module: 'ee3131c', matricno: 'a0101234b', role:'student', group: 1, username: 'bernerdtan', password: 'bernerdtan01' },
+  { id: 3, name: 'Thomas', module: 'ee2033', matricno: 'a0101234c', role:'student', group: 2, username: 'thomasbong', password: 'thomasbong01' },
+  { id: 4, name: 'Ken', module: 'ee4218', matricno: 'a0101234d', role:'student', group: 2, username: 'kenlim', password: 'kenlim01' },
+  { id: 5, name: 'Claudia', module: 'ee2033', matricno: 'a0101234e', role:'student', group: 3, username: 'claudiawong', password: 'claudiawong01' }
 ];
 
-let csv = 'name,age,location\n';
+let csv = 'id,name,module,matricno,role,group,username,password\n';
 
 data.forEach(function(row) {
-  csv += row.name + ',' + row.age + ',' + row.location + '\n';
+  csv += row.id + ',' + row.name + ',' + row.module + ',' + row.matricno + ',' + row.role + ',' + row.group + ',' + row.username + ',' + row.password + '\n';
 });
 
 fs.writeFileSync('data.csv', csv);
