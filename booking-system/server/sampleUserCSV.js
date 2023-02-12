d1182cd83cb8a33b67a450497812b8273eda0d7b
whitecompass <116876873+whitecompass@users.noreply.github.com>
2023-02-12 11:14:24 +0800

    Update sampleUserCSV.js

--- booking-system/server/sampleUserCSV.js
+++ booking-system/server/sampleUserCSV.js
@@ -1,17 +1,17 @@
 const fs = require("fs");
 
 const sampleData = [
-  { id: 1, name: 'Karen', module: 'ee2033', matricno: 'a0101234a', role:'student', group: 2, username: 'karenthia', password: 'karenthia01' },
-  { id: 2, name: 'Bernerd', module: 'ee3131c', matricno: 'a0101234b', role:'student', group: 1, username: 'bernerdtan', password: 'bernerdtan01' },
-  { id: 3, name: 'Thomas', module: 'ee2033', matricno: 'a0101234c', role:'student', group: 2, username: 'thomasbong', password: 'thomasbong01' },
-  { id: 4, name: 'Ken', module: 'ee4218', matricno: 'a0101234d', role:'student', group: 2, username: 'kenlim', password: 'kenlim01' },
-  { id: 5, name: 'Claudia', module: 'ee2033', matricno: 'a0101234e', role:'student', group: 3, username: 'claudiawong', password: 'claudiawong01' }
+  { id: 1, StudentName: 'Karen', Module: 'ee2033', MatricNo: 'a0101234a', Group: 2, Role:'student', Username: 'karenthia', Password: 'karenthia01' },
+  { id: 2, StudentName: 'Bernerd', Module: 'ee3131c', MatricNo: 'a0101234b', Group: 1, Role:'student', Username: 'bernerdtan', Password: 'bernerdtan01' },
+  { id: 3, StudentName: 'Thomas', Module: 'ee2033', MatricNo: 'a0101234c', Group: 2, Role:'student', Username: 'thomasbong', Password: 'thomasbong01' },
+  { id: 4, StudentName: 'Ken', Module: 'ee4218', MatricNo: 'a0101234d', Group: 2, Role:'student', Username: 'kenlim', Password: 'kenlim01' },
+  { id: 5, StudentName: 'Claudia', Module: 'ee2033', MatricNo: 'a0101234e', Group: 3, Role:'student', Username: 'claudiawong', Password: 'claudiawong01' }
 ];
 
-let csv = 'id,name,module,matricno,role,group,username,password\n';
+let csv = 'id,StudentName,Module,MatricNo,Group,Role,Username,Password\n';
 
 data.forEach(function(row) {
-  csv += row.id + ',' + row.name + ',' + row.module + ',' + row.matricno + ',' + row.role + ',' + row.group + ',' + row.username + ',' + row.password + '\n';
+  csv += row.id + ',' + row.StudentName + ',' + row.Module + ',' + row.MatricNo + ',' + row.Group + ',' + row.Role + ',' + row.Username + ',' + row.Password + '\n';
 });
 
 fs.writeFileSync('data.csv', csv);
