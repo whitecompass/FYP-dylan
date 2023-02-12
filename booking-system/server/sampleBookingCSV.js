a4fe8f98c2319af9994d32019833a6d50f8fb5dd
whitecompass <116876873+whitecompass@users.noreply.github.com>
2023-02-12 11:06:38 +0800

    Update sampleBookingCSV.js

--- booking-system/server/sampleBookingCSV.js
+++ booking-system/server/sampleBookingCSV.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 
 const sampleData = [
-  { id: 1, group: 2, Start_time: '23/03/19 14:00', End_time: '23/03/19 17:00', Duration: '3' },
-  { id: 2, group: 1, Start_time: '24/03/19 13:00', End_time: '24/03/19 15:00', Duration: '2' },
-  { id: 3, group: 2, Start_time: '24/03/19 09:00', End_time: '24/03/19 13:00', Duration: '4' },
-  { id: 4, group: 2, Start_time: '25/03/19 13:00', End_time: '25/03/19 14:00', Duration: '1' },
-  { id: 5, group: 3, Start_time: '26/03/19 12:00', End_time: '26/03/19 13:00', Duration: '1' }
+  { id: 1, Group: 2, Start_time: '23/03/19 14:00', End_time: '23/03/19 17:00', Duration: '3' },
+  { id: 2, Group: 1, Start_time: '24/03/19 13:00', End_time: '24/03/19 15:00', Duration: '2' },
+  { id: 3, Group: 2, Start_time: '24/03/19 09:00', End_time: '24/03/19 13:00', Duration: '4' },
+  { id: 4, Group: 2, Start_time: '25/03/19 13:00', End_time: '25/03/19 14:00', Duration: '1' },
+  { id: 5, Group: 3, Start_time: '26/03/19 12:00', End_time: '26/03/19 13:00', Duration: '1' }
 ];
 
 let csv = 'id,Group,Start_time,End_time,Duration\n';
