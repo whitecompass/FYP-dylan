6844ceba9f37cdadfc4c0e1b47fefd8bd1a08c33
whitecompass <116876873+whitecompass@users.noreply.github.com>
2023-02-20 16:04:45 +0800

    Update userdata_insert.js

--- booking-system/server/userdata_insert.js
+++ booking-system/server/userdata_insert.js
@@ -1,5 +1,12 @@
 const mysql = require('mysql');
 
+const db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'root',
+    database: 'test'
+  });
+
 const userData = [
   ["Tester", "ee4218", "a0123456x", 3, "Student", "tester@org.com", "testerpass"],
   ["John", "ee2033", "a111111x", 2, "Student", "john@org.com", "johnpass"],
