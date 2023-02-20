9c0dea03c17c1aae64486c9699c58cbcd3ef0f02
whitecompass <116876873+whitecompass@users.noreply.github.com>
2023-02-20 15:45:23 +0800

    Update insert.js

--- booking-system/server/insert.js
+++ booking-system/server/insert.js
@@ -6,22 +6,9 @@ const userData = [
   ["Michael", "ee4218", "NA", 0, "Teacher", "michael@org.com", "michaelpass"]
 ];
 
-const bookingData = [
-  [2, "03/21/2023 02:00pm", "03/21/2023 03:00pm", 1],
-  [3, "03/22/2023 02:00pm", "03/22/2023 04:00pm", 2]
-];
-
-
 const sql1 = `INSERT INTO userdata (StudentName, Module, MatricNo, Grp, Role, Username, Password) VALUES ?`;
-let sql2 = `INSERT INTO bookings (Grp, Start_time, End_time, Duration) VALUES ?`;
 
 db.query(sql1, [userData], (err, results, fields) {
-  if (err) {
-    console.log(err.message);
-  }
-});
-
-db.query(sql2, [bookingData], (err, results, fields) {
   if (err) {
     console.log(err.message);
   }
